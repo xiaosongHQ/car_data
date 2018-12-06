5e03bbecb2172316edbec8e5a2bde41f603ca1bc
xiaosongHQ <1520339193@qq.com>
2018-12-06 19:22:08 +0800

    234

--- app/car.class.php
+++ app/car.class.php
@@ -6,9 +6,26 @@
             $arr = range("A","Z");
             foreach ($arr as $key => $value) {
                 $body = $this->get_request("https://www.autohome.com.cn/grade/carhtml/{$value}.html");
-                preg_match_all('/h3-tit\"><a href=\"\/(.*?)\">/', $body,$array);
+                preg_match_all('/dl([\s\S]*?)<\/dl/', $body['body'],$array);
+                foreach ($array as $kk => $vv) {
+                    return $vv[0];
+                }
+                return $array;
+
+
+
+                array_shift($array);
+                $car_data = [];
+                foreach ($array[0] as $k => $v) {
+                    $car_data[$k][] = $array[0][$k];
+                    $car_data[$k][] = $array[1][$k];
+                    $car_data[$k][] = $array[2][$k];
+                }
                 return $array;
             }
             return json_encode($arr);
         }
     }
+
+
+/*<dt><a href=\"\/(.*?)\"><img width=\"50\" height=\"50\" src=\"\/(.*?)"><\/a><div><a href=.*?>(.*?)<*/
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once(__DIR__.'/app/car.class.php');
  $car = new Car();
+ echo "<pre>";
     $res = $car->get_car();
     var_dump($res);
